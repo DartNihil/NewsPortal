2b08dff67a2c19f12b8eed6dba1eee3e87ce3243
Stonewall1 <impression97p@gmail.com>
2022-11-16 07:49:31 +0300

    createPost added

--- src/main/java/com/example/newsportal/service/PostService.java
+++ src/main/java/com/example/newsportal/service/PostService.java
@@ -1,21 +1,32 @@
 package com.example.newsportal.service;
 
+import com.example.newsportal.dto.PostDto;
 import com.example.newsportal.entity.Post;
 import com.example.newsportal.repository.PostRepository;
+import com.example.newsportal.service.mapper.PostMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 @Service
+@Transactional
 public class PostService {
     private final PostRepository postRepository;
+    private final PostMapper postMapper;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, PostMapper postMapper) {
         this.postRepository = postRepository;
+        this.postMapper = postMapper;
     }
 
-    public void save(Post post) {
+    public Post save(Post post) {
         post.setCreationTime(LocalDateTime.now());
         postRepository.save(post);
+        return post;
+    }
+
+    public Post mapPostDto(PostDto postDto) {
+        return postMapper.convertPostDto(postDto);
     }
 }
